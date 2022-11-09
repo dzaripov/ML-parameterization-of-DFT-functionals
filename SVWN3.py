72fb0789a57347e6847f310737a2178387c72592
dzaripov <daniszaripov02@mail.ru>
2022-11-09 18:35:04 +0300

    added lots of features

--- SVWN3.py
+++ SVWN3.py
@@ -1,17 +1,11 @@
 import numpy as np
 
+def constants_naming(c_arr):
 
-A_vwn  = [ 0.0310907, 0.01554535]
-b_vwn  = [ 3.72744,   7.06042]
-c_vwn  = [12.9352,    18.0578]
-x0_vwn = [-0.10498,  -0.32500]
-
-A_rpa  = [ 0.0310907,  0.01554535,  -1/(6*np.pi**2)]
-b_rpa  = [13.0720,    20.1231,      1.06835  ]
-c_rpa  = [42.7198,   101.578,      11.4813   ]
-x0_rpa = [-0.409286,  -0.743294,   -0.228344 ]
-
-params_a_alpha = 1
+    labels = ['A_vwn', 'b_vwn', 'c_vwn', 'x0_vwn', 'A_rpa', 'b_rpa', 'c_rpa', 'x0_rpa', 'params_a_alpha']
+    values = [c_arr[0:2],c_arr[2:4],c_arr[4:6],c_arr[6:8],c_arr[8:11],c_arr[11:14],c_arr[14:17],c_arr[17:20], c_arr[20]]
+    
+    return {labels[i]: values[i] for i in range(len(labels))}
 
 
 #VWN
@@ -49,32 +43,32 @@ def f_aux(A, b, c, x0, rs):
   - f2_vwn(b, c, x0)*np.log((np.sqrt(rs) - x0)**2/fx_vwn(b, c, rs)))
 
 
-def DMC(rs, z):
-    return f_aux(A_vwn[1], b_vwn[1], c_vwn[1], x0_vwn[1], rs) \
-    - f_aux(A_vwn[0], b_vwn[0], c_vwn[0], x0_vwn[0], rs)
+def DMC(rs, z, consts):
+    return f_aux(consts['A_vwn'][1], consts['b_vwn'][1], consts['c_vwn'][1], consts['x0_vwn'][1], rs) \
+    - f_aux(consts['A_vwn'][0], consts['b_vwn'][0], consts['c_vwn'][0], consts['x0_vwn'][0], rs)
 
-def DRPA(rs, z):
-    return f_aux(A_rpa[1], b_rpa[1], c_rpa[1], x0_rpa[1], rs) \
-    - f_aux(A_rpa[0], b_rpa[0], c_rpa[0], x0_rpa[0], rs)
+def DRPA(rs, z, consts):
+    return f_aux(consts['A_rpa'][1], consts['b_rpa'][1], consts['c_rpa'][1], consts['x0_rpa'][1], rs) \
+    - f_aux(consts['A_rpa'][0], consts['b_rpa'][0], consts['c_rpa'][0], consts['x0_rpa'][0], rs)
 
 #VWN3
 
 def f_zeta(z): # - power threshold
     return ((1 + z)**(4/3) + (1 - z)**(4/3) - 2)/(2**(4/3) - 2)
 
-def f_vwn(rs, z):
-  return f_aux(A_vwn[0], b_vwn[0], c_vwn[0], x0_vwn[0], rs) \
-  + DMC(rs, z)/DRPA(rs, z)*f_aux(A_rpa[2], b_rpa[2], c_rpa[2], x0_rpa[2], rs)*f_zeta(z)*(1 - z**4)/fpp_vwn \
-  + DMC(rs, z)*f_zeta(z)*z**4
+def f_vwn(rs, z, consts):
+  return f_aux(consts['A_vwn'][0], consts['b_vwn'][0], consts['c_vwn'][0], consts['x0_vwn'][0], rs) \
+  + DMC(rs, z, consts)/DRPA(rs, z, consts)*f_aux(consts['A_rpa'][2], consts['b_rpa'][2], consts['c_rpa'][2], consts['x0_rpa'][2], rs) \
+  * f_zeta(z)*(1 - z**4)/fpp_vwn + DMC(rs, z, consts)*f_zeta(z)*z**4
 
 def rs_z_calc(rho):
     rs = (3/((rho[0] + rho[1]) * (4 * np.pi))) ** (1/3)
     z = (rho[0] - rho[1]) / (rho[0] + rho[1])
     return rs, z
 
-def f_vwn3(rho):
+def f_vwn3(rho, consts):
     rs, z = rs_z_calc(rho)
-    return f_vwn(rs, z)
+    return f_vwn(rs, z, consts)
 
 
 #SLATER
@@ -84,15 +78,17 @@ RS_FACTOR = (3/(4*np.pi))**(1/3)
 DIMENSIONS = 3
 
 
-def f_lda_x(rs, z): # - screen_dens threshold
-    return params_a_alpha*lda_x_spin(rs, z) + params_a_alpha*lda_x_spin(rs, -z)
+def f_lda_x(rs, z, consts): # - screen_dens threshold
+    return consts['params_a_alpha']*lda_x_spin(rs, z) + consts['params_a_alpha']*lda_x_spin(rs, -z)
 
 def lda_x_spin(rs, z):
     return LDA_X_FACTOR*(z+1)**(1 + 1/DIMENSIONS)*2**(-1-1/DIMENSIONS)*(RS_FACTOR/rs)
 
-def f_slater(rho):
+def f_slater(rho, consts):
     rs, z = rs_z_calc(rho)
-    return f_lda_x(rs, z)
+    return f_lda_x(rs, z, consts)
 
-def f_svwn3(rho):
-    return f_slater(rho) + f_vwn3(rho)
+def f_svwn3(rho, constants_array):
+    '''rho.shape = (2, 1)'''
+    consts = constants_naming(constants_array)
+    return f_slater(rho, consts) + f_vwn3(rho, consts)
